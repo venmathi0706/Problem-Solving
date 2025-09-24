d9758eca23791950944914309d58fd6aa268de8e
venmathi0706 <117349046+venmathi0706@users.noreply.github.com>
2025-09-24 12:39:55 +0530

    Create 242. Valid anagram.java

---
+++ 23.09.2025/242. Valid anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] arr=new int[27];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        for(int c:arr){
+            if(c!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
